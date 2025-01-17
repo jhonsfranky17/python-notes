bce761f7cb939afde9d78f0d02b41a1e1cc18e33
Franklin Jetty Johnson <franklinjettyjohnson6@gmail.com>
2025-01-17 11:49:39 +0530

    math functions

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-#This is my first Python program
+# this is my first python program
 # print("Hello guys!")
 # print("I like Biriyani") 
 
-#Variables and types
+# variables and types
 
-#Strings
+# strings
 # first_name = "Franklin"
 # food = "Biriyani"
 # email = "franklinjettyjohnson6@gmail.com"
@@ -14,19 +14,16 @@
 # print(f"I like {food}")
 # print(f"My email is {email}")
 
-#Integers
-
+# integers
 # age = 24 
 # print(f"I am {age} years old")
 
-#Float
-
+# float
 # price = 10.99
 
 # print(f"The price is {price}")
 
-#Boolean
-
+# boolean
 # is_student = True
 
 # if is_student:
@@ -34,8 +31,8 @@
 # else:
 #     print("You are not a student")
 
-#typecasting - the process of converting a variable from one data type to another
-#str(), int(), float(), bool()
+# typecasting - the process of converting a variable from one data type to another
+# str(), int(), float(), bool()
 
 # name = "Franklin Jetty Johnson"
 # age = 24
@@ -48,13 +45,35 @@
 # print(type(age))
 # print(age)
 
-#input() = A funtion that prompts the user to enter data. Returns the enteres data as a string
+# input() = A funtion that prompts the user to enter data. Returns the enteres data as a string
 
 # name = input("Enter your name: ")
-# age = int(input("Enter your age: ")) #typecasting entered string type to int
+# age = int(input("Enter your age: ")) # typecasting entered string type to int
 
 # age += 1
 
 # print(f"Hello {name}")
 # print("Happy Birthday!")
 # print(f"You are {age} years old")
+
+# built-in math functions
+
+# x = 3.14
+# y = -1
+# z = 5
+
+# result = round(x) # rounds off to the nearest integer
+# result = abs(y) # returns the value without sign
+# result = pow(x, 3) # returns the base times the power
+# result = max(x, y, z) # returns the maximum among the values
+# result = min(x, y, z) # returns the minimum among the values
+
+# print(result)
+
+# import math
+
+# print(math.pi) # returns the value of pi
+# print(math.e) # returns the value of e
+# print(math.sqrt(36)) # returns the square root of a number
+# print(math.ceil(9.1)) # returns the rounded up value
+# print(math.floor(9.9)) # returns the rounded down value
