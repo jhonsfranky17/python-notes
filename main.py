519170b9a117f5c4bc2220adf17767fa9676797f
Franklin Jetty Johnson <franklinjettyjohnson6@gmail.com>
2025-01-17 11:06:20 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+#This is my first Python program
+# print("Hello guys!")
+# print("I like Biriyani") 
+
+#Variables and types
+
+#Strings
+# first_name = "Franklin"
+# food = "Biriyani"
+# email = "franklinjettyjohnson6@gmail.com"
+
+# print(first_name)
+# print(f"Hello {first_name}")
+# print(f"I like {food}")
+# print(f"My email is {email}")
+
+#Integers
+
+# age = 24 
+# print(f"I am {age} years old")
+
+#Float
+
+# price = 10.99
+
+# print(f"The price is {price}")
+
+#Boolean
+
+# is_student = True
+
+# if is_student:
+#     print("You are a student")
+# else:
+#     print("You are not a student")
+
+#typecasting - the process of converting a variable from one data type to another
+#str(), int(), float(), bool()
+
+# name = "Franklin Jetty Johnson"
+# age = 24
+# gpa = 3.2
+# is_student = True
+
+# age = str(age)
+# age += "1"
+
+# print(type(age))
+# print(age)
+
+#input() = A funtion that prompts the user to enter data. Returns the enteres data as a string
