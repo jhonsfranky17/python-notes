991085b2e14a585840a55f4cb409d813e94aadf2
Franklin Jetty Johnson <franklinjettyjohnson6@gmail.com>
2025-01-18 10:18:43 +0530

    chore: Add conditional expression to main.py

--- main.py
+++ main.py
@@ -130,4 +130,10 @@
 #     print("It is warm outside")
 #     print("It is cloudy")
 
+# conditional expression = A one-line shortcut for the if-else statement (ternary operator)
+#                          Print or assign one of two values based on a condition
+#                          X if condition else Y
 
+num = 5
+
+print("Positive" if num > 0 else "Negative")
