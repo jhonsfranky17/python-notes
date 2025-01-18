721febe95787f95801b16f7e375d3357db3e1e00
Franklin Jetty Johnson <franklinjettyjohnson6@gmail.com>
2025-01-18 11:23:37 +0530

    Refactor main.py to add string formatting with format specifiers

--- main.py
+++ main.py
@@ -173,3 +173,24 @@
 # print(credit_number[-1]) # returns the last character, negative indices return characters from the end
 # print(credit_number[::3]) # returns every 3rd character starting from the first character (included)
 # print(credit_number[::-1]) # reverses the string, add -1 at the end to reverse the given string
+
+# format specifiers = {value:flags} format a value based on what flags are inserted
+
+# .(number)f = round to that many decimal places (fixed point)
+# :(number) = allocate that many spaces
+# :03 = allocate and zero pad that many spaces
+# :< = left justify
+# :> = right justify
+# :^ = center align
+# :+ = use a plus sign to indicate positive value
+# := = place sign to leftmost position
+# : = insert a space before positive numbers
+# :, = comma separator
+
+# price1 = 3.14159
+# price2 = -987.65
+# price3 = 12.23
+
+# print(f"Price 1 is {price1:.1f}")
+# print(f"Price 2 is {price2:.1f}")
+# print(f"Price 3 is {price3:.1f}")
