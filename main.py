094d3ded18b32399eeb858bc31a79b84da3fb996
Franklin Jetty Johnson <franklinjettyjohnson6@gmail.com>
2025-01-18 10:13:22 +0530

    chore: Refactor main.py to include logical operators

--- main.py
+++ main.py
@@ -89,3 +89,45 @@
 #     print("You are not born yet!")  
 # else:
 #     print("Sorry, you are not eligible to vote in INDIA")
+
+# logical operators = evaluate multiple conditions (or, and , not)
+# or = at least one condition must be True
+# and = both conditions must be True
+# not = inverts the condition (not False, not True)
+
+# logical or = or
+# temp = 30
+# is_raining = False
+
+# if temp > 35 or temp < 0 or is_raining:
+#     print("The outdoor event is cancelled")
+# else:
+#     print("The outdoor event is still scheduled")
+
+# logical and = and
+
+# is_sunny = True
+
+# if temp >=28 and is_sunny:
+#     print("It is hot outside")
+#     print("It is sunny")
+# elif temp <=0 and is_sunny:
+#     print("It is cold outside")
+#     print("It is sunny")
+# elif 28 > temp > 0 and is_sunny:
+#     print("It is warm outside")
+#     print("It is sunny")
+
+# logical not = not
+
+# if temp >=28 and not is_sunny:
+#     print("It is hot outside")
+#     print("It is cloudy")
+# elif temp <=0 and not is_sunny:
+#     print("It is cold outside")
+#     print("It is cloudy")
+# elif 28 > temp > 0 and not is_sunny:
+#     print("It is warm outside")
+#     print("It is cloudy")
+
+
