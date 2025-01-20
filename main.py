ba9dd31f47b514faa5cc2cd515481722ade906fc
Franklin Jetty Johnson <franklinjettyjohnson6@gmail.com>
2025-01-20 18:14:56 +0530

    Refactor main.py to include match-case statement for improved code readability and maintainability

--- main.py
+++ main.py
@@ -486,3 +486,39 @@
 # print(positive_numbers)
 # print(negative_numbers)
 
+# match-case statement (switch) = an alternative to using many 'elif' statements
+#                                 execute some code if a value matches a 'case'
+#                                 benefits: cleaner and syntax is more readable 
+
+
+# def day_of_week(day):
+#     match day:
+#         case 1:
+#             print("Sunday")
+#         case 2:
+#             print("Monday")
+#         case 3:
+#             print("Tuesday")
+#         case 4:
+#             print("Wednesday")
+#         case 5:
+#             print("Thursday")
+#         case 6:
+#             print("Friday")
+#         case 7: 
+#             print("Saturday")
+#         case _: # '_' is considered as a wildcard, if no other case is true, this case will be triggered
+#             print("Not a valid day")
+
+# day_of_week(1)
+
+# def is_weekend(day):
+#     match day:
+#         case "Saturday" | "Sunday":
+#             return True
+#         case "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday":
+#             return False
+#         case _:
+#             return False
+
+# print(is_weekend("Monday"))
