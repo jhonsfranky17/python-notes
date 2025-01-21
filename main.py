8a2a13b5fc476e834b82d1479422a8a58b8f7837
Franklin Jetty Johnson <franklinjettyjohnson6@gmail.com>
2025-01-21 17:53:38 +0530

    Refactor main.py to include super() for improved code structure and inheritance

--- main.py
+++ main.py
@@ -716,3 +716,42 @@
 
 # rabbit.eat()
 
+# super() = function used in a child class to call methods from a parent class(superclass).
+#           allows you to extend the functionality of the inherited methods
+
+# class Shape:
+#     def __init__(self, color, filled):
+#         self.color = color
+#         self.filled = filled
+        
+# class Circle(Shape):
+#     def __init__(self, color, filled, radius):
+#         super().__init__(color, filled)
+#         self.radius = radius
+
+# class Square(Shape):
+#     def __init__(self, color, filled, width):
+#         super().__init__(color, filled)
+#         self.width = width
+
+# class Triangle(Shape):
+#     def __init__(self, color, filled, width, height):
+#         super().__init__(color, filled)
+#         self.width = width
+#         self.height = height
+
+# circle = Circle("red", True, 5)
+# print(circle.color)
+# print(circle.filled)
+# print(circle.radius)
+
+# square = Square("green", True, 5)
+# print(square.color)
+# print(square.filled)
+# print(square.width)
+
+# triangle = Triangle("yellow", True, 5, 10)
+# print(triangle.color)
+# print(triangle.filled)
+# print(triangle.width)
+# print(triangle.height)
