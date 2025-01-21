d84edfff143efe187a56c34997e085ee6bd54b8d
Franklin Jetty Johnson <franklinjettyjohnson6@gmail.com>
2025-01-21 17:37:46 +0530

    Refactor main.py to include multiple inheritance for code reusability and extensibility

--- main.py
+++ main.py
@@ -666,3 +666,53 @@
 # cat.speak()
 # mouse.speak()
 
+# multiple inheritance = child class inherits from more than one parent class
+#                        C(A,B)
+# multilevel inheritance = child class inherits from a parent class which in turn inherits from another parent
+#                          C(B) <- B(A) <- A
+
+# class Animal:
+
+#     def __init__(self, name):
+#         self.name = name
+
+#     def eat(self):
+#         print(f"{self.name} is eating")
+#     def sleep(self):
+#         print(f"{self.name} is sleeping")
+
+# class Prey(Animal):
+#     def flee(self):
+#         print(f"{self.name} is fleeing")
+
+# class Predator(Animal):
+#     def hunt(self):
+#         print(f"{self.name} is hunting")
+
+# class Rabbit(Prey):
+#     pass
+
+# class Hawk(Predator):
+#     pass
+
+# class Fish(Prey, Predator): # multiple inheritance
+#     pass
+
+# rabbit = Rabbit("Bugs")
+# hawk = Hawk("Tony")
+# fish = Fish("Nemo")
+
+# rabbit.flee()
+# hawk.hunt()
+
+# fish.hunt()
+# fish.flee()
+
+# rabbit.eat()
+# rabbit.sleep()
+
+# fish.eat()
+# fish.sleep()
+
+# rabbit.eat()
+
